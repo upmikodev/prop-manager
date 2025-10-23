0c6976f88e40077cb7b941d6511bbee6b85f32de
upmikodev <michaelgraham176@outlook.com>
2025-10-22 21:04:28 -0600

    Vercel Speed Insights

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,10 @@
 // src/index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,5 +13,6 @@ root.render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
