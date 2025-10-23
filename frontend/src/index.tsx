865c4175c5a236702bd151fcac893a9ef13477a0
upmikodev <michaelgraham176@outlook.com>
2025-10-22 19:58:13 -0600

    Vercel Analytics

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,5 +11,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
